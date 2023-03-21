139b74d03465c93469fefa658c6ff165f9720259
minseo kim <halia0709@gmail.com>
2023-03-21 09:34:59 +0900

    참조로 바꾸기

--- 문법/참조풀기.cpp
+++ 문법/참조풀기.cpp
@@ -1,13 +1,13 @@
 ﻿#include <iostream>
 using namespace std;
-void 더하라2를(int* num)
+void 더하라2를(int& num)
 {
-	*num += 2;
+	num += 2;
 }
 int main(void)
 {
 	int a = 3;
-	더하라2를(&a);
+	더하라2를(a);
 	cout << a << endl;
 	return 0;
 }
