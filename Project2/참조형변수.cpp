284707f157372d3283012d0958d1abfe83572a50
minseo kim <halia0709@gmail.com>
2023-03-21 09:05:15 +0900

    포인터

--- Project2/참조형변수.cpp
+++ Project2/참조형변수.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
 int main(void) {
+	int a = 30;			//a:30
+
+	int& ra = a;		//pa : a의 주소값
+	ra = 100;			//pa가 가르키는 대상(a) : 100
+	cout << "a의 값은" << a << endl;
+
 	return 0;
 }
