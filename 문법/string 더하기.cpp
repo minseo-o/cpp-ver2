03e7014e26f4c63103926e8ac466cfc6607f73c8
minseo-o <halia0709@gmail.com>
2023-03-23 11:01:35 +0900

    C스타일 문자열 합치기

---
+++ 문법/string 더하기.cpp
@@ -0,0 +1,16 @@
+﻿//C
+#include <stdio.h>
+#include <string.h>
+//c++
+#include <iostream>
+#include <string>
+int main(void) {
+	char str1[30] = "minseo";
+	char str2[30] = "kim";
+	const int str1_l = strlen(str1);
+	const int str2_l= strlen(str2);
+	for (int i = strlen(str1); i < str1_l + str2_l; i++) {
+		str1[i] = str2[i - str1_l];
+	}
+	printf("%s \n", str1);
+}
