19fefcf22cb561652a15e980d557bf4f3b8da8fd
minseo-o <halia0709@gmail.com>
2023-03-23 11:04:07 +0900

    c 스타일 strcat() 문자열 더하기

--- 문법/string 더하기.cpp
+++ 문법/string 더하기.cpp
@@ -7,10 +7,7 @@
 int main(void) {
 	char str1[30] = "minseo";
 	char str2[30] = "kim";
-	const int str1_l = strlen(str1);
-	const int str2_l= strlen(str2);
-	for (int i = strlen(str1); i < str1_l + str2_l; i++) {
-		str1[i] = str2[i - str1_l];
-	}
+	strcat(str1, str2);
+	
 	printf("%s \n", str1);
 }
