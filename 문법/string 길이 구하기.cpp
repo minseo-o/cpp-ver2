7c7aa7d02869780cad6a22403cbbf6cc39c4dfcb
minseo-o <halia0709@gmail.com>
2023-03-23 10:47:18 +0900

    c스타일 문자열길이

--- 문법/string 길이 구하기.cpp
+++ 문법/string 길이 구하기.cpp
@@ -1,15 +1,11 @@
-﻿#include <iostream>
-using namespace std;
-
-int sum(int a, int b) {
-	return a + b;
-}
-float sum(float a, float b) {
-	return a + b;
-}
-int main(void)
-{
-	cout << sum_float(1.1f, 2.2f) << endl;
-	cout << sum_int(1,2) << endl;
+﻿#include <stdio.h>
+#include <String.h>
+int main(void) {
+	char str[30] = "minseo";
+	int size = 0;
+	for (int i = 0; str[i] != '\0'; i++) {
+		size++;
+	}
+	printf("문자열의 크기는 %d \n", size);
 	return 0;
 }
