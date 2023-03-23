9c3328ce260c3e64927f1f07308aff99a14aeee0
minseo-o <halia0709@gmail.com>
2023-03-23 10:41:56 +0900

    c스타일 문자열 길이 구하기

---
+++ 문법/string 길이 구하기.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int sum(int a, int b) {
+	return a + b;
+}
+float sum(float a, float b) {
+	return a + b;
+}
+int main(void)
+{
+	cout << sum_float(1.1f, 2.2f) << endl;
+	cout << sum_int(1,2) << endl;
+	return 0;
+}
