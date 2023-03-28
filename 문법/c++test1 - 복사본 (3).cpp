689c2b619975778363134c98872bae51cb926a96
minseo-o <halia0709@gmail.com>
2023-03-28 09:47:51 +0900

    c++ 테스트 2

---
+++ 문법/c++test1 - 복사본 (3).cpp
@@ -0,0 +1,17 @@
+﻿//C
+#include <stdio.h>
+#include <string.h>
+//c++
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+	
+	string str1 = "HELLO";
+	string str2 = "";
+	str2 = str1;
+	cout << str2 << endl;
+}
---
+++ 문법/c++test1 - 복사본.cpp
@@ -0,0 +1,19 @@
+﻿//C
+#include <stdio.h>
+#include <string.h>
+//c++
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+	
+
+    string str = "minseo";
+    cout << "문자열의 길이는 " << str.size() << endl;
+ 
+   
+
+}
