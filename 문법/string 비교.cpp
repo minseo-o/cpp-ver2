7cf195241df9f9a9286040eb5aa950c04ff6022f
minseo-o <halia0709@gmail.com>
2023-03-28 09:10:41 +0900

    C스타일 strcmp 문자열 비교 헷갈려

--- 문법/string 비교.cpp
+++ 문법/string 비교.cpp
@@ -11,31 +11,15 @@ int main(void) {
 	char str1[50] = "Hello";
 	char str2[50] = "Aorld";
 
-	const int s1_l = strlen(str1);
-	const int s2_l = strlen(str2);
-
-	const int l = s1_l < s2_l ? s1_l : s2_l;
-	for (int i = 0; i < l; i++) {
-		if (str1[i] < str2[i]) {
-			printf("str2 > str1 \n");
-			break;
-		}
-		else if (str1[i] > str2[i]) {
-			printf(" str1 >str2 \n");
-			break;
-		}
+	int result = strcmp(str1, str2);
+	if (result == -1) {
+		printf("str2 > str1");
 	}
-	int i;
-	if (i == l) {
-		if (s1_l > s2_l) {
-			printf("str2 <str1 \n");
-		}
-		else if (s1_l <s2_l) {
-			printf("str2 <str1 \n");
-		}
-		else {
-			printf("str2 == str1 \n");
-		}
+	else if (result == 1) {
+		printf("str2 < str1");
+	}
+	else if (result == 0) {
+		printf("str2 == str1");
 	}
 	
 	
