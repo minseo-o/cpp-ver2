aea4a27fd7005f68f1bea98355560706844a355b
minseo-o <halia0709@gmail.com>
2023-03-28 09:04:44 +0900

    C스타일 string 비교

---
+++ 문법/string 비교.cpp
@@ -0,0 +1,42 @@
+﻿//C
+#include <stdio.h>
+#include <string.h>
+//c++
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+	char str1[50] = "Hello";
+	char str2[50] = "Aorld";
+
+	const int s1_l = strlen(str1);
+	const int s2_l = strlen(str2);
+
+	const int l = s1_l < s2_l ? s1_l : s2_l;
+	for (int i = 0; i < l; i++) {
+		if (str1[i] < str2[i]) {
+			printf("str2 > str1 \n");
+			break;
+		}
+		else if (str1[i] > str2[i]) {
+			printf(" str1 >str2 \n");
+			break;
+		}
+	}
+	int i;
+	if (i == l) {
+		if (s1_l > s2_l) {
+			printf("str2 <str1 \n");
+		}
+		else if (s1_l <s2_l) {
+			printf("str2 <str1 \n");
+		}
+		else {
+			printf("str2 == str1 \n");
+		}
+	}
+	
+	
+}
