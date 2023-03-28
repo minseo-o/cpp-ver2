931a59c7a1115ee346d7fffc2a26c44a68dcf441
minseo-o <halia0709@gmail.com>
2023-03-28 09:59:11 +0900

    test4

---
+++ 문법/c++test4.cpp
@@ -0,0 +1,25 @@
+﻿//C
+#include <stdio.h>
+#include <string.h>
+//c++
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+	
+		char str1[50] = "Hello";
+		char str2[50] = "Hello";
+		
+
+		if (str2 == str1) {
+			cout << str1<<"같다"<<str2 <<endl;
+		}
+		else {
+			cout << str1 << "다르다" << str2 << endl;
+		}
+		
+
+}
