6e284fe33590b19567e7c364f266370e15c90a62
minseo-o <halia0709@gmail.com>
2023-05-18 11:19:45 +0900

    [sfml] 상하좌우 키 완성

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -17,9 +17,13 @@ int main() {
 				window.close();
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Up))
-			snake.move(0, -1);
+			snake.move(0, -5);
 		if (Keyboard::isKeyPressed(Keyboard::Down))
-			snake.move(0, 1);
+			snake.move(0, 5);
+		if (Keyboard::isKeyPressed(Keyboard::Right))
+			snake.move(5, 0);
+		if (Keyboard::isKeyPressed(Keyboard::Left))
+			snake.move(-5, 0);
 		window.clear();
 		window.draw(snake);
 		window.display();
