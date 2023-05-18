8ea4e670f59fab5fb0d334d66890a6e6433b86d2
minseo-o <halia0709@gmail.com>
2023-05-18 11:15:13 +0900

    [sfml] 프레임 조절

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -2,6 +2,9 @@
 using namespace sf;
 int main() {
 	RenderWindow window(VideoMode(640,480),"Snake Game");
+	window.setFramerateLimit(60);
+	//1초에 60번 작업이 이루어 지도록 frame 조절
+	//컴퓨터 사양이 달라도 똑같은 속도로 처리함
 	RectangleShape snake;
 	snake.setPosition(200, 300);
 	snake.setSize(Vector2f(30,30));
