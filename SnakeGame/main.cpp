fff621ef0ff7cbd388f13f81a841d10394ae30f3
minseo-o <halia0709@gmail.com>
2023-05-18 11:05:00 +0900

    [sfml] 움직여보자

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -2,12 +2,23 @@
 using namespace sf;
 int main() {
 	RenderWindow window(VideoMode(640,480),"Snake Game");
+	RectangleShape snake;
+	snake.setPosition(200, 300);
+	snake.setSize(Vector2f(30,30));
+	snake.setFillColor(Color::Green);
+
 	while (window.isOpen()) {
 		Event e;
 		while (window.pollEvent(e)) {
-
+			if (e.type == Event::Closed)
+				window.close();
 		}
+		if (Keyboard::isKeyPressed(Keyboard::Up))
+			snake.move(0, -1);
+		if (Keyboard::isKeyPressed(Keyboard::Down))
+			snake.move(0, 1);
 		window.clear();
+		window.draw(snake);
 		window.display();
 	}
 	return 0;
