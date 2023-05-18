05062f897af2408ea35d93c10b63ffdb12421d4e
minseo-o <halia0709@gmail.com>
2023-05-18 10:11:08 +0900

    sfml 잘 돌아가나 확인

---
+++ SnakeGame/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <SFML/Graphics.hpp>
+using namespace sf;
+int main() {
+	RenderWindow window(VideoMode(640,480),"Snake Game");
+	while (window.isOpen()) {
+		Event e;
+		while (window.pollEvent(e)) {
+
+		}
+		window.clear();
+		window.display();
+	}
+	return 0;
+}
---
+++ 문법/복사생성자.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Student
+{
+private:
+    int nHakbun;
+    char* sName;
+
+public:
+    Student();
+    Student(int Hakbun, const char* Name);
+    Student(const Student& rhs);
+    ~Student();
+
+    void show();
+};
+
+Student::Student()
+{
+}
+
+Student::Student(int Hakbun, const char* Name)
+    : nHakbun(Hakbun)
+{
+    cout << "일반생성자 호출." << endl;
+    int len = strlen(Name) + 1;
+    sName = new char[len];
+    strcpy(sName, Name);
+}
+
+Student::Student(const Student& rhs)
+    : nHakbun(rhs.nHakbun)
+{
+    int len = strlen(rhs.sName) + 1;
+    sName = new char[len];
+    strcpy(sName, rhs.sName);
+}
+
+Student::~Student()
+{
+    delete[] sName;
+    cout << "소멸자 호출" << endl;
+}
+
+void Student::show()
+{
+    cout << "학번은 " << nHakbun << "입니다" << endl;
+    cout << "이름은 " << sName << "입니다" << endl << endl;
+}
+
+int main(void)
+{
+    Student stu1 = Student(1111, "JWP");
+    Student stu2 = stu1;
+
+    stu1.show();
+    stu2.show();
+
+    return 0;
+}
